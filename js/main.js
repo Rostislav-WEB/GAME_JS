a008358ce0241e6b03a634103b9efed0e7c738b4
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-06-23 23:44:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function startGame() {
 		enemy.y = -100 * setting.traffic * (i + 1);
 		enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
 		enemy.style.top = enemy.y + 'px';
-		enemy.style.background = 'transparent url(\'../img/enemy3.png\') center  / cover no-repeat'
+		enemy.style.background = 'transparent url("/img/enemy3.png") center  / cover no-repeat'
 		gameArea.appendChild(enemy);
 	}
 
@@ -109,3 +109,4 @@ function moveEnemy() {
 	});
 }
 // item.y = -150 * setting.traffic;
+// путь с ../ на /
